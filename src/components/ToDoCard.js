55d74bdeaeb9cd91bf07b0ddf821fc385b7ae5a9
Moiz Imran <moiz.imran@tintash.com>
2018-02-27 14:13:47 +0500

    Fixed a bug in the 'completed' checkbox

--- src/components/ToDoCard.js
+++ src/components/ToDoCard.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const ToDoCard = ({ title, text, onCompleted, onRemoved }) => (
+const ToDoCard = ({ title, text, onCompleted, onRemoved, completed }) => (
     <div>
         <h3>{title}</h3>
         <p>{text}</p>
-        Completed: <input type='checkbox' onChange={onCompleted} /> 
+        Completed: <input type='checkbox' onChange={onCompleted} checked={completed} /> 
         <button onClick={onRemoved}>Remove</button>
     </div>
 )
